df045f15dad14698af1d2d1d02e5b198d8690442
33hello33 <pqviet94@gmail.com>
2022-11-01 00:11:23 +0700

    change to vue3 in invoice.js

--- static/assets/js/invoice.js
+++ static/assets/js/invoice.js
@@ -1,34 +1,33 @@
-var Vue = new Vue({
-    el: '#root',
+const {createApp} = Vue;
+appInvoice = createApp({
     delimiters: ['@{', '}'],
-    data: {
-      customer: {name: '', phone: '', address:''},
-      invoice:{customer_phone: '', total_money: 0, had_paid:0},
-      product: {id: 0, name: '', unit: '', amount: 0, price: 0, price_import: 0, warehouse: '', id_supplier: 0},
-      products: [],
-      productTbl: {id: 0, name: '', unit: '', amount: 0, price: 0, total_price: 0, discount: 0, last_price: 0},
-      productTbls: [],
-      resetKey: 0,
-      total_money_to_pay: 0,
-    },
-    mounted(){
-      this.listProduct();
+    data() {
+      return {
+        customer: {name: '', phone: '', address:''},
+        invoice:{customer_phone: '', total_money: 0, had_paid:0},
+        product: {id: 0, name: '', unit: '', amount: 0, price: 0, price_import: 0, warehouse: '', id_supplier: 0},
+        products: [],
+        productTbl: {id: 0, name: '', unit: '', amount: 0, price: 0, total_price: 0, discount: 0, last_price: 0},
+        productTbls: [],
+        resetKey: 0,
+        total_money_to_pay: 0,
+      }
     },
     methods: {
       listProduct(){
-        this.$http.get('product/list').then(response =>{
+        axios.get('product/list').then(response =>{
           if(response.status == 200){
-            this.products = response.body;
+            this.products = response.data;
             this.product = {id: 0, name: '', unit: '', amount: 0, price: 0, price_import: 0, warehouse: '', id_supplier: 0};
           }else{
-            console.log(response.body);
+            console.log(response.data);
           }
         });
       },
       getDetailProduct(product, productIndex){
-        this.$http.get('product/' + product.id).then(response =>{
+        axios.get('product/' + product.id).then(response =>{
           if (response.status == 200){
-            this.productTbl = response.body;
+            this.productTbl = response.data;
             this.productTbl.amount = 1;
             this.productTbl.discount = 0;
             this.productTbl.total_price = this.productTbl.price;
@@ -59,18 +58,18 @@ var Vue = new Vue({
         // send info customer, if not exist, create new customer
         this.invoice.customer_phone = this.customer.phone;
         this.invoice.total_money = this.total_money_to_pay;
-        this.$http.post('customer', this.customer).then(response =>{
+        axios.post('customer', this.customer).then(response =>{
           if(response.status = 200){
             // send info invoice, all product of invoice
-            this.$http.post('invoice', {"invoice" : this.invoice, "products": this.productTbls}).then(response =>{
+            axios.post('invoice', {"invoice" : this.invoice, "products": this.productTbls}).then(response =>{
               if(response.status = 200){
                 this.reset();
               }else{
-                console.log(response.body);
+                console.log(response.data);
               }
             })
           }else{
-            console.log(response.body);
+            console.log(response.data);
           }
         })
       },
@@ -78,4 +77,10 @@ var Vue = new Vue({
         this.productTbls = [];
         this.total_money_to_pay = 0;
       },
-    }});
+    },
+     beforeMount(){
+      this.listProduct();
+    },
+  });
+  
+appInvoice.mount("#root")
